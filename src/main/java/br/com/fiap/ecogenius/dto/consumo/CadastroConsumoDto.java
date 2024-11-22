278504e46dacbe456879aefc8a447dbf63e51187
SunaUezuri <wesley.jane.santos@gmail.com>
2024-11-22 09:05:51 -0300

    Java finalizado

---
+++ src/main/java/br/com/fiap/ecogenius/dto/consumo/CadastroConsumoDto.java
@@ -0,0 +1,24 @@
+package br.com.fiap.ecogenius.dto.consumo;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter @Setter
+public class CadastroConsumoDto {
+
+    @NotNull
+    private double previsao; //Em porcentagem
+    @NotBlank @Size(max = 50)
+    private String condicaoClimatica;
+    @NotNull
+    private int horario; //Somente as horas
+    @NotNull
+    private LocalDate dataPrevisao;
+    @NotNull
+    private int idServico;
+}
